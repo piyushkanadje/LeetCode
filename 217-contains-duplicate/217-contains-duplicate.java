2560e342a5bbcf667f140175d5f27a0766ce7d90
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2022-08-02 11:56:07 +0530

    Time: 29 ms (14.83%), Space: 75.1 MB (5.12%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hs = new HashSet<Integer>();
+     
+        for(int i = 0;i<nums.length;i++){
+            hs.add(nums[i]);
+        }
+        if(nums.length > hs.size()){
+            return true;
+        }
+        return false;
+    }
+}
