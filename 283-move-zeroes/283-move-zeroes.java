da025416a03db38fbae96f5ebab8a8b710895d38
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2022-09-16 05:15:29 +0530

    Time: 13 ms (14.19%), Space: 55.1 MB (20.00%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int zeroes = 0 ;
+        for(int i = 0 ; i <nums.length; i++){
+            if(nums[i] == 0 ){
+                zeroes++;
+            }else{
+            int temp =  nums[i];
+            nums[i] = 0 ;
+            nums[i-zeroes] = temp;
+            }
+           
+            
+            
+        }
+    }
+        
+}
