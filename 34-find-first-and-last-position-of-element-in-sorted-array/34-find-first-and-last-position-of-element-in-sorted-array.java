afc166200f2d4dd97afbc2f345e5349ad6f6bead
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2022-09-17 01:34:14 +0530

    Time: 0 ms (100.00%), Space: 47.1 MB (67.98%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,18 +1,46 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int[] arr = new int[] {-1,-1};
-        
-        
-        for(int i = nums.length -1 ; i>=0 ; i--){
-            if(nums[i] == target){
-            arr[0] = i;
-            }
-        }
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == target){
-                arr[1] =i;
-            }
-        }
-        return arr;
+    public  int[] searchRange(int[] nums, int target) {
+int[] ans =  new int[] {firstOcc(nums , target) , last(nums , target)};
+        return ans;
     }
+         public  int firstOcc(int nums[], int target){
+        int  low= 0 ;
+             int high = nums.length -1;
+         while(low <= high){
+         int mid  = (low + high)/2;
+          if(nums[mid] > target){
+              high = mid-1;
+          }else if( nums[mid] <  target){
+              low  =  mid + 1 ; 
+          }    else{
+              if(mid == 0 ||  nums[mid] !=  nums[mid-1] ){
+                  return mid;
+              }else{
+                  high =  mid -1;
+              }
+          }
+         
+        }
+                 return -1;
+    }
+    
+    public  int last(int[] nums, int target){
+            int  low= 0 , high =nums.length -1;
+         while(low <= high){
+         int mid  = (low + high)/2;
+          if(nums[mid] > target){
+              high = mid-1;
+          }else if( nums[mid] <  target){
+              low  =  mid + 1 ; 
+          }else{
+              if(mid == nums.length -1 || nums[mid] !=nums[mid+1]){
+                  return mid;
+              }else{
+                  low = mid+1;
+              }
+          }
+             
+    }
+        return -1;
+}
 }
