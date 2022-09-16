5b8bb903f77c7382f7ba185294eccb717afeb368
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2022-09-17 02:35:38 +0530

    Time: 1 ms (100.00%), Space: 39.7 MB (88.66%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int mySqrt(int x) {
+        long low = 1 ; long high =x; long ans = 0;
+        while(low<=high){
+            long mid =(low+high)/2;
+            long midsq= mid * mid;
+            if(midsq == x){
+                return (int)mid;
+            }else if(midsq > x){
+                high = mid-1;
+                
+            }else{
+                low = mid +1;
+                 ans = mid;
+            }
+            
+        }
+        return (int)ans;
+    }
+}
