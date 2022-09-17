fdcdead930ded10b7ec2335257ad3151aa62c250
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2022-09-18 01:15:07 +0530

    Time: 0 ms (100.00%), Space: 42 MB (88.62%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,46 +1,38 @@
 class Solution {
-    
-    /*n: The pivot always will be in the 0 index.
-if target is greather than pivot we will search from 1 index
-if target is less than pivot we will search from n-1 index. */
-    public int search(int[] nums, int target) { 
-    
-       int fix = nums[0];
-       int n = nums.length;
-    
+    public int search(int[] nums, int target) {
+        int fix = nums[0];
+        int n = nums.length;
         
-        if(target <fix){
-            int right = n -1;
+        if(target < fix){
+            int right  = n-1;
             while(true){
-                if(right >= 0 && target == nums[right]){
+            if(right >= 0 && target == nums[right]){
                     return right;
                 }
                 
-                if(right < 0 || nums[right] < target){
-                    return -1;
+                if( right< 0 || nums[right]  < target){
+                      return -1;
                 }
-                right -- ;
+                right--;
             }
-        }
-        
- else if(target > fix){  
-         int left  = 1;
-            
+        }else if(target > fix){
+            int left =1; 
             while(true){
-            if(left<=n-1  && target == nums[left]){
-                return left;
+              if(left<=n-1 && nums[left] == target){
+                  return left;
+              }
+            if(left > n -1 || nums[left] > target){
+                return -1;
             }
-                if(left >= n || target < nums[left]){
-return -1;
+                left++;
             }
-            left ++; 
+            
+        }else{
+            if(target == fix){
+                return 0;
             }
-             
-         }else if(target == fix){
-
- return  0 ; }
-        
-return -1;
-    
+                
+        }
+        return -1;
     }
 }
