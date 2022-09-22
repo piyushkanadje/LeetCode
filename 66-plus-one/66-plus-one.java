dd75649e557c9028cbbf050c14559d678b522018
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2022-09-21 22:27:19 -0700

    Time: 0 ms (100.00%), Space: 42.7 MB (25.13%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,19 +1,15 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int carry=1;
-        for(int n=digits.length-1;n>=0;n--){
-            digits[n]=digits[n]+carry;
-            carry=digits[n]/10;
-            if(digits[n]>9){
-                digits[n]%=10;
+        for(int  i = digits.length -1 ; i >=0; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
             }
+                digits[i] =0;
+            
         }
-        if(carry>0){
-            int[] result=new int[digits.length+1];
-            result[0]=1;
-            System.arraycopy(digits,0,result,1,digits.length);
-            return result;
-        }
-        return digits;
+        int[] arr = new int[digits.length+ 1];
+        arr[0] = 1;
+        return arr;
     }
 }
