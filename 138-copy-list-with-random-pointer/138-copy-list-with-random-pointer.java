c337c2b85161fdb01241f5529236bcd2daa50d7e
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2022-10-02 22:24:13 -0700

    Time: 1 ms (55.82%), Space: 46.3 MB (6.50%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -15,19 +15,28 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-     HashMap<Node, Node> map = new HashMap<Node , Node>();
+     
+        //First add all element in HashMap Like <Node , Node>  so first             will be the curr and second will be new Node(curr.val)
         
-     for(Node curr = head; curr != null; curr= curr.next){
-         map.put(curr, new Node(curr.val));
-     }
-for(Node curr = head; curr != null; curr= curr.next){
-    Node clone =  map.get(curr);
-    clone.next = map.get(curr.next);
-    clone.random = map.get(curr.random);
- }
+        HashMap<Node, Node> map = new HashMap<Node, Node>();
+        
+        for(Node curr =  head; curr !=  null ; curr =curr.next){
+            map.put(curr, new Node(curr.val));
+        }
+        
+        // create a new clone Node and attached element to it
+        
+        for(Node curr =  head ; curr!= null; curr = curr.next){
+            Node clone =  map.get(curr);
+            clone.next = map.get(curr.next);
+            clone.random = map.get(curr.random);
+        }
         
         Node head2 = map.get(head);
+        
         return head2;
-    
+        
+        
+        
     }
 }
