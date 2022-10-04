14236c7e73e7e27e31c3e3641c62cd67f8e7ce3e
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2022-10-04 14:53:32 -0700

    Time: 9 ms (35.10%), Space: 63.6 MB (70.72%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.java
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.java
@@ -1,34 +1,33 @@
 class Solution {
     
-    //Kadens algorith
-    public static int normalMax(int[] a){
-        int n =a.length;
-        int res =a[0]; int maxEnd = a[0];
-        for(int i = 1;i< n ;i++){
-            maxEnd  = Math.max(maxEnd +a[i] , a[i]);
-            res = Math.max(res , maxEnd);
+     // normal sum 
+        
+        static int normal(int[] nums){
+            
+            int res = nums[0];
+            int maxEnd = nums[0];
+            for(int i = 1 ; i < nums.length; i++){
+                maxEnd = Math.max(maxEnd + nums[i], nums[i]);
+                res =  Math.max(maxEnd , res);
+            }
+            return res;
         }
-        return res;
-        
-    }
-    
-    
-    
-    public int maxSubarraySumCircular(int[] a) {
-         int n =a.length;
-        int normalSum = normalMax(a);
-        
-        //check weather normal sum is less than  zero if yes return normal sum
+    public int maxSubarraySumCircular(int[] nums) {
+     
+        int normalSum = normal(nums);
         if(normalSum < 0){
             return normalSum;
         }
-//arrSum is minimum sum of subarray
+        
         int arrSum = 0;
-        for(int i= 0 ;i< n ; i++){
-          arrSum += a[i];
-            a[i] = -a[i];
+        for(int i = 0; i < nums.length; i++){
+            arrSum += nums[i];
+            nums[i] = - nums[i];
         }
-        int maxCircularSum = arrSum + normalMax(a);
+        
+         int maxCircularSum = arrSum + normal(nums);
         return Math.max(normalSum , maxCircularSum);
+        
+       
     }
 }
