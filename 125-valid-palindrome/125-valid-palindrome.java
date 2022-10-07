b2f9de409542347b26cb8f9ddfe73957fdf1d702
Piyush Prabhakar Kanadje <41730532+piyushkanadje@users.noreply.github.com>
2022-10-07 13:17:54 -0700

    Time: 3 ms (99.18%), Space: 41.9 MB (99.05%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,32 +1,32 @@
 class Solution {
     public boolean isPalindrome(String s) {
-       String fixed_string = "";
-        
-        //create fixed string with alpha numeric and letter only
-        for(char c :  s.toCharArray()){
-            if(Character.isDigit(c) || Character.isLetter(c)){
-                fixed_string +=c;
+      // Base case
+        if(s==null)  return false;
+        // Take in lowercase character...
+        s = s.toLowerCase();
+        // Initialize two pointer variables, left and right and point them with the two ends of the input string...
+        int left = 0;
+        int right = s.length()-1;
+        // Traverse all elements through the loop...
+        while(left<right){
+            // Move the left pointer to right so it points to a alphanumeric character...
+            while(left<right && !((s.charAt(left)>='a' && s.charAt(left)<='z') || (s.charAt(left)>='0'&&s.charAt(left)<='9'))){
+                left++;
             }
-        }
-        
-    // to lower case
-        fixed_string =  fixed_string.toLowerCase();
-        
-        
-    // iterate through string
-         
-        int first = 0;
-        int last = fixed_string.length() - 1;
-        
-        while(first <= last){
-            if(fixed_string.charAt(first) != fixed_string.charAt(last))             {
+            // Similarly move right pointer to left so it also points to a alphanumeric character...
+            while(left<right && !((s.charAt(right)>='a' && s.charAt(right)<='z') || (s.charAt(right)>='0'&&s.charAt(right)<='9'))){
+                right--;
+            }
+            // Check if both the characters are same...
+            // If it is not equal then the string is not a valid palindrome, hence return false...
+            if(s.charAt(left) != s.charAt(right)){
                 return false;
-                
             }
-            first++;
-            last--;
+            // If same, then continue to next iteration and move both pointers to point to next alphanumeric character till left < right...
+            left++;
+            right--;
         }
-        return true;
-        
+        // After loop finishes, the string is said to be palindrome, hence return true...
+        return true;  
     }
 }
